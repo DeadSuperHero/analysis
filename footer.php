ccbee180d302af0ef85d7bfa5a29e8ab3352a05a
Sean Tilley <sean@deadsuperhero.com>
2019-10-22 21:55:37 -0700

    fixes

--- footer.php
+++ footer.php
@@ -18,14 +18,10 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'analysis' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'analysis' ), 'WordPress' );
+				printf( esc_html__( 'Begrudgingly developed with %s', 'analysis' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'analysis' ), 'analysis', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
