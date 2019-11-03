51f2dc6e3802895eb20543441deaf8966eada249
Sean Tilley <sean@deadsuperhero.com>
2019-11-03 15:43:52 -0800

    attempt wrapping comment box

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,53 +10,54 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<div class="article_insert">
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+	<div class="article_insert">
+		<header class="entry-header">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
 
-		if ( 'post' === get_post_type() ) :
+			if ( 'post' === get_post_type() ) :
+				?>
+
+				<?php analysis_post_thumbnail(); ?>
+
+				<div class="entry-meta">
+					<?php
+					analysis_posted_on();
+					analysis_posted_by();
+					?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'analysis' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'analysis' ),
+				'after'  => '</div>',
+			) );
 			?>
 
-			<?php analysis_post_thumbnail(); ?>
+			<?php
+				comments_template();
+			?>
 
-			<div class="entry-meta">
-				<?php
-				analysis_posted_on();
-				analysis_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'analysis' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'analysis' ),
-			'after'  => '</div>',
-		) );
-		?>
-
-		<footer class="entry-footer">
-			<?php analysis_entry_footer(); ?>
-		</footer><!-- .entry-footer -->
-	</div><!-- .entry-content -->
-</div>
+		</div><!-- .entry-content -->
+	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
