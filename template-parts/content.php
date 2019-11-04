b33640a51c582f9ac50c34f8ba047170d36ea95a
Sean Tilley <sean@deadsuperhero.com>
2019-11-03 21:13:21 -0800

    do something really risky

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,23 +12,27 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="article_insert">
 		<header class="entry-header">
-			<?php
-			if ( is_singular() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
 
-			if ( 'post' === get_post_type() ) :
-				?>
-
-				<div class="entry-meta">
+				<div class="article-begin">
+					<?php analysis_post_thumbnail(); ?>
 					<?php
-					analysis_posted_on();
-					?>
-				</div><!-- .entry-meta -->
+					if ( is_singular() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
 
-				<?php analysis_post_thumbnail(); ?>
+					if ( 'post' === get_post_type() ) :
+						?>
+
+						<div class="entry-meta">
+							<?php
+							analysis_posted_on();
+							?>
+						</div><!-- .entry-meta -->
+
+				</div>
+
 			<?php endif; ?>
 		</header><!-- .entry-header -->
 
