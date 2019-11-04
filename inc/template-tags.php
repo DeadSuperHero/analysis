066fed567158d8095e97b0e046c14978965f8076
Sean Tilley <sean@deadsuperhero.com>
2019-11-03 19:27:18 -0800

    fix post date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -26,7 +26,7 @@ if ( ! function_exists( 'analysis_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x('post date', 'analysis' ),
+			esc_html_x( '%s', 'post date', 'analysis' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
