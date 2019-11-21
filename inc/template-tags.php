260b5f36613a0e32e4fcdf7380dc62fb202cdc3a
Sean Tilley <sean@deadsuperhero.com>
2019-11-20 23:54:13 -0600

    add tags glyph

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -71,7 +71,7 @@ if ( ! function_exists( 'analysis_entry_footer' ) ) :
 			 $tags_list = get_the_tag_list( '', esc_html_x( ' ', 'list item separator', 'analysis' ) );
 			 if ( $tags_list ) {
 				 /* translators: 1: list of tags. */
-				 printf( '<span class="tags-links">' . esc_html__( '%1$s', 'analysis' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+				 printf( '<span class="tags-links">' . '<i class="fa fa-tags" aria-hidden="true"></i>' . esc_html__( '%1$s', 'analysis' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			 }
 		 }
 	 }
