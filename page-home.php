444c2161ff554579fd3e65c9016a5edfbc8139f8
Sean Tilley <sean@deadsuperhero.com>
2019-11-21 16:47:28 -0600

    more skeleton

--- page-home.php
+++ page-home.php
@@ -23,7 +23,10 @@ get_header();
 				<button class="primary">It's OK</button>
 			</div>
 
+			<hr></hr>
+
 			<div id="recent-updates">
+				<h2>Recent Updates</h2>
 				<div id="latest-status">
 					<p>More fake stuff here.</p>
 				</div>
