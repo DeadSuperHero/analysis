49d37958b0fa45c623a1b2d0777594359695b7db
Sean Tilley <sean@deadsuperhero.com>
2019-11-21 16:23:38 -0600

    flip tag list on indexes again

--- template-parts/content-preview.php
+++ template-parts/content-preview.php
@@ -42,10 +42,11 @@
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
+			<?php the_excerpt(); ?>
+
 			<span class="tags-list">
 				<?php analysis_tags_list(); ?>
 			</span>
-			<?php the_excerpt(); ?>
 
 		</div><!-- .entry-content -->
 	</div>
