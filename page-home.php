2e3a634055390af5a41f3ea04decce09052961e2
Sean Tilley <sean@deadsuperhero.com>
2019-11-21 16:31:47 -0600

    strip home content

--- page-home.php
+++ page-home.php
@@ -19,20 +19,6 @@ get_header();
 		<main id="main" class="site-main">
 			<p>If you're seeing this, that means you're on the home page.</p>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
