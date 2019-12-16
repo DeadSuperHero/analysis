770fbba870ad4823b160869dc0270e2633bbeb6e
Sean Tilley <sean@deadsuperhero.com>
2019-12-16 13:44:27 -0600

    add thumb

--- posts_loop_template.php
+++ posts_loop_template.php
@@ -8,6 +8,7 @@
 
 	<!-- This is the output of the excerpt -->
 	<div class="entry-summary">
+    <?php analysis_post_thumbnail(); ?>
 		<?php the_excerpt(); ?>
 	</div>
 
@@ -15,9 +16,8 @@
 	<div class="entry-utility">
 		<?php if ( count( get_the_category() ) ) : ?>
 			<span class="cat-links">
-				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+				<?php printf( __( '<span class="%1$s"></span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
 			</span>
-			<span class="meta-sep">|</span>
 		<?php endif; ?>
 	</div>
 </div>
