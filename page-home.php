a046f471a96d5bbc121609ba8358014b2699505f
Sean Tilley <sean@deadsuperhero.com>
2019-12-17 00:30:44 -0600

    move down in DOM

--- page-home.php
+++ page-home.php
@@ -29,9 +29,8 @@ get_header();
 				<h2><i class="fa fa-newspaper-o" aria-hidden="true"></i> From the Blog</h2>
 				<div class="latest_posts">
 					<?php echo do_shortcode("[ic_add_posts showposts='6']") ?>
-
-					<a href="https://deadsuperhero.com/blog" class="action-link"><i class="fa fa-chevron-right" aria-hidden="true"></i> Read More</a>
 				</div>
+				<a href="https://deadsuperhero.com/blog" class="action-link"><i class="fa fa-chevron-right" aria-hidden="true"></i> Read More</a>
 				<div id="latest-statuses">
 					<h2><i class="fa fa-globe" aria-hidden="true"></i> From the Web</h2>
 					<p>Coming soon...</p>
